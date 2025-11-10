a0658cc043d823222f4f9fd484f57424ed922e8b
omoefe <raniem57@gmail.com>
2025-11-10 19:34:03 +0100

    removed

--- app/page.js
+++ app/page.js
@@ -4,13 +4,11 @@ import Hero from "@/components/home/Hero";
 import Projects from "@/components/home/Project";
 import Services from "@/components/home/ServicesSection";
 import Testimonials from "@/components/home/Testimonials";
-import NotAvailable from "@/components/NotAvailable";
 import React from "react";
 
 const Home = () => {
   return (
     <div className="text-primary ">
-      <NotAvailable />
       <Hero />
       <AboutUs />
       <Services />
